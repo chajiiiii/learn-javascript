15b5206643897222a52f4f6455923804306287a4
chajiiiii <j.cha1708@gmail.com>
2025-06-10 11:07:20 +0900

    Number 데이터 타입

--- learn/03-primitives.js
+++ learn/03-primitives.js
@@ -13,3 +13,16 @@
 "큰 따옴표 문자 값";
 // 어퍼스트로피(이스케이프 처리)
 console.log("나의 100m 달리기 최고 기록은 12'26\"이야.");
+
+// ---------------------------
+// Number
+console.log("1,000,000");
+console.log(1_000_000);
+
+// 나머지(modulus) : %
+// 10000을 6으로 나누고 남는 값
+console.log(10000 % 6);
+
+// JS 숫자(정수)의 안전 범위 표현
+console.log(Number.MIN_SAFE_INTEGER);
+console.log(Number.MAX_SAFE_INTEGER);
