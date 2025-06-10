c1d656ff269fede1708107050d97dbcad382444e
chajiiiii <j.cha1708@gmail.com>
2025-06-10 13:06:01 +0900

    실습

--- learn/03-primitives.js
+++ learn/03-primitives.js
@@ -64,3 +64,28 @@ console.log(Symbol("고유 식별자"));
 console.log(BigInt(Number.MAX_SAFE_INTEGER + 10));
 // 숫자 뒤에 n만 붙여도 BigInt로 인식
 console.log(10000n);
+
+// ---------------------------
+
+// # 실습
+
+// 콘솔 패널에서 숫자, 문자열, 불리언 값을 만들어 다음을 연습합니다.
+
+// 1. 문자열(String) 만들고 출력하기
+console.log("Hi");
+// 2. 문자열 두 개 합치기
+console.log("Hi" + " there");
+// 3. 숫자(Number) 만들고 출력하기
+console.log(8);
+// 4. `31`과 `81` 더하기
+console.log(31 + 81);
+// 5. `502`에서 `271` 빼기
+console.log(502 - 271);
+// 6. `5`와 `117` 곱하기
+console.log(5 * 117);
+// 7. `100`을 `8`로 나누기
+console.log(100 / 8);
+// 8. `502`를 `3`으로 나눈 나머지 구하기
+console.log(502 % 3);
+// 9. 불리언(Boolean) 만들고 출력하기
+console.log(true);
