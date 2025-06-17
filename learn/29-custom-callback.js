3687c82dff81d770e9e356c30e46179aaae82053
chajiiiii <j.cha1708@gmail.com>
2025-06-17 09:18:03 +0900

    [학습] 커스텀 콜백(함수)

---
+++ learn/29-custom-callback.js
@@ -0,0 +1,23 @@
+function callToAction(callback) {
+  callback();
+}
+
+callToAction(() => {
+  console.log("콜백은 다른 함수에 인수로 전달되는 함수를 말합니다.");
+});
+
+// ---------------------------------
+// click 함수 작성
+
+const button = document.querySelector(".clickable");
+// button.addEventListener("click", () => {
+//   console.log("button clicked");
+// });
+
+const handleClick = () => console.log("clicked button");
+
+click(button, handleClick);
+
+function click(element, listener) {
+  element.addEventListener("click", listener);
+}
