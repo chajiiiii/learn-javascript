aed26843f9d988e5bf3116c4df39cf017e8a2d18
chajiiiii <j.cha1708@gmail.com>
2025-06-19 14:35:54 +0900

    [실습] Loop

--- assets/scripts/learn/33-loop.js
+++ assets/scripts/learn/33-loop.js
@@ -257,3 +257,67 @@ while (wantToDraw) {
     console.log(key, value);
   }
 }
+
+// --------------------------------------------------------------------------
+// 실습
+
+(() => {
+  // 컴퓨터 프로그래밍의 기본 구조 중 하나인 루프에 숙달하기 위한 연습을 진행하세요.
+
+  // 1. for 문을 사용해 `1`부터 `100`까지의 숫자 중, 짝수만 출력
+  // for (let num = 1; num < 101; num++) {
+  //   if (num % 2 === 0) console.log(num);
+  // }
+
+  // 2. while 문을 사용해 숫자 `10`부터 `1`까지 역순으로 출력
+  let num2 = 10;
+
+  while (num2 > 0) {
+    console.log(num2);
+    num2--;
+  }
+
+  // 3. for … of 문을 사용해 다음 배열의 과일 출력
+  const fruits = ["사과", "바나나", "귤", "복숭아"];
+
+  for (const fruit of fruits) console.log(fruit);
+
+  // 4. do … while 문을 사용해 입력된 비밀번호(`'3001'`) 매칭 검토 후 결과 출력
+  let i = 0,
+    input;
+  const attempts = ["0124", "8291", "3001", "9073"];
+
+  do {
+    input = attempts[i];
+
+    if (input === "3001") {
+      console.log("일치");
+      break;
+    }
+
+    i = i + 1;
+  } while (i < attempts.length);
+
+  // 5. for … in 문을 사용해 객체의 key, value 출력
+  const student = {
+    name: "선호",
+    grade: "A",
+    age: 22,
+  };
+
+  for (const key in student) {
+    let value = student[key];
+    console.log(key, ":", value);
+  }
+
+  // 6. `1`부터 `20`까지 반복
+  //     1. `5`부터 `10`까지는 건너띄고 나머지 출력
+  //     2. `17`이 되면 반복 종료
+  for (let num3 = 1; num3 < 20; num3++) {
+    if (num3 >= 5 && num3 <= 10) {
+      continue;
+    }
+    if (num3 === 17) break;
+    console.log(num3);
+  }
+})();
